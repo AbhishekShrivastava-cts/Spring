bee53596b99213a38f575825e3a2364c8a670d02
AbhishekShrivastava-cts <60421165+AbhishekShrivastava-cts@users.noreply.github.com>
2020-03-05 16:46:41 +0530

    Add files via upload

---
+++ iiht/IIHTtest2Spring/src/main/java/com/cts/IihTtest2SpringApplication.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IihTtest2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IihTtest2SpringApplication.class, args);
+	}
+
+}
